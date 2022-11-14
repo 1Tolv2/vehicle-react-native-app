8f57ed4cce9f228679b1ca16b4f1a98156bbbc45
1Tolv2 <sofiaq1@gmail.com>
2022-11-14 16:12:15 +0100

    formatted mainbutton

--- src/components/atoms/MainButton.jsx
+++ src/components/atoms/MainButton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Pressable, Text, StyleSheet } from "react-native";
-import theme from '../theme/index';
+import theme from "../theme/index";
 
 const { colors } = theme;
 
@@ -18,49 +18,47 @@ const MainButton = ({ type, title, event, bgColor, brandColor, disabled }) => {
     <>
       {type === "outlined" ? (
         <Pressable
-        style={styles({bgColor, fill: false}).container}
+          style={styles({ bgColor, fill: false }).container}
           onPress={event}
           type={type}
           bgColor={disabled ? "lightgrey" : renderColor(bgColor, brandColor)}
         >
-          <Text style={styles({fill: false}).text}>
-            {title}
-          </Text>
+          <Text style={styles({ fill: false }).text}>{title}</Text>
         </Pressable>
       ) : (
         <Pressable
-        style={styles({bgColor, fill: true}).container}
+          style={styles({ bgColor, fill: true }).container}
           onPress={event}
           type={type}
           bgColor={disabled ? "lightgrey" : renderColor(bgColor, brandColor)}
         >
-          <Text style={styles({fill: true}).text}>{title}</Text>
+          <Text style={styles({ fill: true }).text}>{title}</Text>
         </Pressable>
       )}
     </>
   );
 };
 
-const styles = ({bgColor, fill}) => StyleSheet.create({
+const styles = ({ bgColor, fill }) =>
+  StyleSheet.create({
     container: {
-        marginVertical: 10,
-        marginHorizontal: 0,
-        paddingHorizontal: 5,
-        paddingTop: 10,
-        paddingBottom: 12,
-        borderRadius: 10,
-        width: "100%",
-        backgroundColor: fill ? colors[bgColor] : "transparent",
-        borderColor: colors[bgColor],
-        borderWidth: fill ? 0 : 1,
+      marginVertical: 20,
+      marginHorizontal: 0,
+      paddingHorizontal: 5,
+      paddingTop: 10,
+      paddingBottom: 12,
+      borderRadius: 10,
+      width: "100%",
+      backgroundColor: fill ? colors[bgColor] : "transparent",
+      borderColor: colors[bgColor],
+      borderWidth: fill ? 0 : 1,
     },
     text: {
-        textAlign: "center",
-        color: fill ? colors.white : colors[bgColor],
-        fontSize: 16,
-        fontWeight: "bold",
-    }
-});
-
+      textAlign: "center",
+      color: fill ? colors.white : colors[bgColor],
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+  });
 
 export default MainButton;
