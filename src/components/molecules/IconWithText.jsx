028a3c34c5813967fb0f9be0211fdcdf19d13c21
1Tolv2 <sofiaq1@gmail.com>
2022-11-14 19:36:56 +0100

    moved iconwithtext out of folder

--- src/components/molecules/IconWithText.jsx
+++ src/components/molecules/IconWithText.jsx
@@ -1,23 +1,23 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
-import RegularText from "../../atoms/RegularText";
-import theme from "../../theme";
+import RegularText from "../atoms/RegularText";
+import theme from "../theme";
 
 const { colors } = theme;
 
 const Icons = {
-  power: require("../../../../assets/icons/speed_icon.png"),
-  mileage: require("../../../../assets/icons/directions_car.png"),
-  nextInspection: require("../../../../assets/icons/select_check_box.png"),
+  power: require("../../../assets/icons/speed_icon.png"),
+  mileage: require("../../../assets/icons/directions_car.png"),
+  nextInspection: require("../../../assets/icons/select_check_box.png"),
 };
-const RenderIcon = (icon) => {
+const renderIcon = (icon) => {
   return <Image style={styles({ isMiddle: false })} source={Icons[icon]} />;
 };
 
 const IconWithText = ({ icon, text, isMiddle }) => {
   return (
     <View style={styles({ isMiddle }).container}>
-      {RenderIcon(icon)}
+      {renderIcon(icon)}
       <RegularText margin={0}>{text}</RegularText>
     </View>
   );
