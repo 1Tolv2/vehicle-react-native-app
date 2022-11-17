9f17c0084520daac51c855f510e5b084e63ee943
1Tolv2 <sofiaq1@gmail.com>
2022-11-17 21:39:56 +0100

    added text to help error seraching

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const getToken = async () => {
     const jsonValue = await AsyncStorage.getItem("@access_token");
     return jsonValue != null ? JSON.parse(jsonValue) : null;
   } catch (err) {
-    console.log(err.message);
+    console.log("getToken", err.message);
   }
 };
 
@@ -27,7 +27,7 @@ const storeToken = async (value) => {
   try {
     await AsyncStorage.setItem("@access_token", JSON.stringify(value));
   } catch (err) {
-    console.log(err.message);
+    console.log("storeToken", err.message);
   }
 };
 
@@ -35,26 +35,24 @@ export const registerUser = async (username, password, settings) => {
   try {
     return await axios.post("/users", { username, password, settings });
   } catch (err) {
-    console.log(err.message);
+    console.log("registerUser", err.message);
   }
 };
 
 export const loginUser = async (username, password) => {
   try {
     const res = await axios.post("/users/auth", { username, password });
-    storeToken(res.data.token);
+    if (res.data?.token) {
+      storeToken(res.data.token);
+    }
     return res;
   } catch (err) {
-    console.log(err.message);
+    console.log("loginUser", err.message);
   }
 };
 
 export const getUser = async () => {
-  try {
-    return await axios.get("/users/me");
-  } catch (err) {
-    console.log(err.message);
-  }
+  return await axios.get("/users/me");
 };
 
 export const getVehicles = async () => {
@@ -62,6 +60,6 @@ export const getVehicles = async () => {
     const jwt = await getToken();
     return await axios.get("/vehicles");
   } catch (err) {
-    console.log(err.message);
+    console.log("getVehicles", err.message);
   }
 };
