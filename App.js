8ad06626b2083fa4ee4ea089b27b2d7751191d8d
1Tolv2 <sofiaq1@gmail.com>
2022-11-19 23:07:39 +0100

    Removed registrationScreen

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./src/routes/LoginScreen";
-import RegisterScreen from "./src/routes/RegisterScreen";
 import AddVehicleScreen from "./src/routes/AddVehicleScreen";
 import VehicleDetailScreen from "./src/routes/VehicleDetailScreen";
 import HomeScreen from "./src/routes/HomeScreen";
@@ -45,12 +41,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
